680df70612f82c466f5c33ebe05bc0d097ff2f0e
skoanton <antonhakanssonn@gmail.com>
2023-12-22 08:29:40 +0100

    lel

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-const config ={
-    api_key:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NzE5NGY3MDg2MTMzZDBkNTk3Y2U3M2NlZTFjMDA2MSIsInN1YiI6IjY1ODRlZDJhNzFmMDk1NTc3YzIzZDE0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p4xnwJ9BKFIAfVdPkSNhZ_DwzVKcDj06Nz6kkauqmL0',
-}
-
-
-export default config;
